8b39b597216ecd747fa4b2f37d81da5c1838ff01
Sai Kamal <61119148+kamal3344@users.noreply.github.com>
2025-11-21 09:57:30 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,3 @@
+a = 12 # save in the Memory 
+
+print(a) # calling from Memory 
---
+++ log_file.py
@@ -0,0 +1,14 @@
+import logging
+
+def setup_logging(script_name):
+    logger = logging.getLogger(script_name)
+    if not logger.handlers:
+        logger.setLevel(logging.DEBUG)
+        # Create a file handler for the script
+        handler = logging.FileHandler(f'C:\\Users\\DELL\\Downloads\\9_30_Ml_P\\credit_card\\logs\\{script_name}.log', mode='w')
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.propagate = False
+
+    return logger
